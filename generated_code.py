20ac83dafcdf3a77445d3c1a8827a403d2b06335
Nikolaos Konstantinou <77862574+nccon@users.noreply.github.com>
2023-06-20 19:20:16 +0300

    Initial commit

---
+++ generated_code.py
@@ -0,0 +1,9 @@
+# Welcome Page Code
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Welcome to My First Automated Project Management Script using AI</title>
+  </head>
+  <body>
+    <h1>Welcome to My First Automated Project Management Script using AI!</h1>
+    <p>This project is designed to make project management easier and more efficient by leveraging the power of artificial intelligence.</p>
